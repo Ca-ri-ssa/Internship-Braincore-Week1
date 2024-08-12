e7abf66a8262ff4ec63c858b94b684567103fd50
carissa-chandra <carissa.tjan03@gmail.com>
2024-08-12 15:43:10 +0700

    Success Added Middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { Elysia } from 'elysia';
 
 const app = new Elysia()
-    .get('/', () => "Hello World")
+    .get('/', () => "Hello World", {
+            beforeHandle() {
+                const time = new Date();
+                const wib = new Date(time.getTime() + (7 * 60 * 60 * 1000)); // Convert to WIB (GMT+7)
+                console.log(`Time Request: ${wib.toISOString()}`);
+            }
+        }
+    )
     .listen(3000)
 
 console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
