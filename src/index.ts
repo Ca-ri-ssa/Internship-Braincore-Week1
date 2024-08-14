4b52d217859d9782529edca2cbf7cfc95760c584
carissa-chandra <carissa.tjan03@gmail.com>
2024-08-14 10:32:20 +0700

    Add Todo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from 'elysia';
 
 const app = new Elysia()
+    // TODO: Create a basic Elysia app with one route, which returns "Hello World"
     .get('/', () => "Hello World", {
+            // TODO: Add middleware to write the time request
             beforeHandle() {
                 const time = new Date();
                 const wib = new Date(time.getTime() + (7 * 60 * 60 * 1000)); // Convert to WIB (GMT+7)
