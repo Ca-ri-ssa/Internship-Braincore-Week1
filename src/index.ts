574c1b13213b110d69cd7e86603e68bd387d9295
carissa-chandra <carissa.tjan03@gmail.com>
2024-08-14 11:25:58 +0700

    Add Files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+    // .use(route)
+    .listen(3000)
+
+console.log(`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`)
